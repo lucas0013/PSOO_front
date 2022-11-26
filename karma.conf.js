d13585408ce285f340a690ff515cf04e55efed05
Lucas dos Santos <lucassantos0013@gmail.com>
2022-11-26 06:09:15 -0300

    Muda nome projeto

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/cliente-contato'),
+      dir: require('path').join(__dirname, './coverage/LojaGeekFront'),
       subdir: '.',
       reporters: [
         { type: 'html' },
